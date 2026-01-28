09bfbbf90801535dd966c7b3830183c3ff9e1279
redbean0721 <redbean0721@gmail.com>
2026-01-28 18:38:36 +0800

    feat: 優化首頁背景顯示效果
    - 啟用首頁全局背景圖片顯示
    - 調整 card/post 透明度以突出背景圖片
    - 修正 layout.ejs 引號格式問題
    
    參考: https://github.com/theme-particlex/hexo-theme-particlex/pull/109

--- themes/particlex/source/js/lib/home.js
+++ themes/particlex/source/js/lib/home.js
@@ -1,10 +1,26 @@
 mixins.home = {
     mounted() {
-        let background = this.$refs.homeBackground;
-        let images = background.dataset.images.split(",");
-        let id = Math.floor(Math.random() * images.length);
-        background.style.backgroundImage = `url('${images[id]}')`;
+        let homeBackgroundRef = this.$refs.homeBackground;
+        if (homeBackgroundRef) {  // 檢查元素是否存在
+            let homeBgImgURLs = homeBackgroundRef.dataset.images.split(",").filter(url => url.trim());
+            if (homeBgImgURLs.length > 0) {  // 檢查是否有有效的 URL
+                homeBackgroundRef.style.backgroundImage = `url('${
+                homeBgImgURLs[Math.floor(Math.random() * homeBgImgURLs.length)]
+                }')`;
+            }
+        }
+
+        // let homeBgImgURLs = homeBackgroundRef.dataset.images.split(",");
+        // homeBackgroundRef.style.backgroundImage = `url('${
+        //   homeBgImgURLs[Math.floor(Math.random() * homeBgImgURLs.length)]
+        // }')`;
         this.menuColor = true;
+
+        let contentBackgroundRef = this.$refs.contentBackground;
+        let contentBgImgURLs = contentBackgroundRef.dataset.images.split(",");
+        contentBackgroundRef.style.backgroundImage = `url('${
+          contentBgImgURLs[Math.floor(Math.random() * contentBgImgURLs.length)]
+        }')`;
     },
     methods: {
         homeClick() {
